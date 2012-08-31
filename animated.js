6c75827d43db78e03344b6c17ca160af3c5182ed
BADBADNOTGOOD <badbadnotgoodemail@gmail.com>
2012-08-31 12:23:51 -0400

    Added options and ability to stop infinite animation

--- animated.js
+++ animated.js
@@ -1,6 +1,20 @@
 (function( $ ) {
   $.fn.animated = function(animation, callback, options) {
     
+    _this = this
+    
+    if (animation == 'stop' && this.data('current-animation')) {
+      $.each(Animated.vendorPrefixFor('iteration-count'), function(k, v) {
+        _this.css(v, '1')
+      })
+      this.removeClass('animated ' + this.data('current-animation'))
+      this.removeData('current-animation')
+      if (jQuery.isFunction(callback)) {
+        callback.apply(null, [this])
+      };
+      return;
+    };
+    
     if (!options) {
       options = {}
     }
@@ -10,42 +24,72 @@
     };
     
     if (!options.time) {
-      options.time = Animated.effects[animation].time ? Animated.effects[animation] : Animated.defaults.time
+      options.time = Animated.effects[animation].time ? Animated.effects[animation].time : Animated.defaults.time
     }
     
-    _this = this
-    
     if (options.duration) {
-      $.each(Animated.vendorPrefixFor('duration'), function() {
-        _this.css(this, options.duration)
+      // Convert duration to milliseconds for timeout
+      options.time = parseInt(options.duration.substring(0, options.duration.length - 1)) * 1000
+      $.each(Animated.vendorPrefixFor('duration'), function(k, v) {
+        _this.css(v, options.duration)
       })
+    }
+    
+    if (options.delay) {
+      $.each(Animated.vendorPrefixFor('delay'), function(k, v) {
+        options.time += parseInt(options.delay.substring(0, options.delay.length - 1)) * 1000
+        _this.css(v, options.delay)
+      })
+    };
+    
+    if (options.iterationCount || options.infinite == true) {
+      options.iterationCount = options.infinite ? 'infinite' : options.iterationCount
+      $.each(Animated.vendorPrefixFor('iteration-count'), function(k, v) {
+        _this.css(v, options.iterationCount)
+      })
+      if (options.iterationCount == 'infinite') {
+        options.infinite = true
+      };
     };
     
     this.addClass("animated " + animation)
     
-    setTimeout(function() {
-      _this.removeClass('animated ' + animation)
-      if (Animated[animation] && Animated[animation].hide == true) {
-        _this.hide();
-      };
-      if (jQuery.isFunction(callback)) {
-        callback.apply(null, [_this])
-      };
-    }, options.time)
-
+    
+    if (!options.infinite) {
+      setTimeout(function() {
+        _this.removeClass('animated ' + animation)
+        if (Animated.effects[animation] && Animated.effects[animation].hide == true) {
+          _this.hide();
+        };
+        if (jQuery.isFunction(callback)) {
+          callback.apply(null, [_this])
+        };
+      }, options.time)
+    } else {
+      this.data('current-animation', animation)
+    }
   }
   
 })( jQuery );
 
 var Animated = {
   effects: {
-    wiggle: {},
-    wobble: {},
-    fade: {hide: true}
+    wiggle: {time: 300},
+    wobble: {time: 300},
+    fadeIn: {},
+    fadeOut: {hide: true},
+    fadeOutUp: {hide: true},
+    fadeOutDown: {hide: true},
+    fadeOutLeft: {hide: true},
+    fadeOutRight: {hide: true},
+    fadeOutUpBig: {hide: true, time: 200},
+    fadeOutDownBig: {hide: true, time: 200},
+    fadeOutLeftBig: {hide: true, time: 200},
+    fadeOutRightBig: {hide: true, time: 200}
   },
   
   defaults: {
-    time: 300,
+    time: 800,
     hide: false,
     show: false
   },
