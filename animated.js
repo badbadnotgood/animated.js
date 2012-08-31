8dd10b9f3d4b4e9601c4d20272eb7082e5e6f28d
BADBADNOTGOOD <badbadnotgoodemail@gmail.com>
2012-08-31 10:14:53 -0400

    Added vendor prefix function

--- animated.js
+++ animated.js
@@ -5,18 +5,30 @@
       options = {}
     }
     
+    if (jQuery.isPlainObject(callback)) {
+      options = callback
+    };
+    
     if (!options.time) {
       options.time = Animated.effects[animation].time ? Animated.effects[animation] : Animated.defaults.time
     }
     
-    this.addClass("animated " + animation)
     _this = this
+    
+    if (options.duration) {
+      $.each(Animated.vendorPrefixFor('duration'), function() {
+        _this.css(this, options.duration)
+      })
+    };
+    
+    this.addClass("animated " + animation)
+    
     setTimeout(function() {
       _this.removeClass('animated ' + animation)
       if (Animated[animation] && Animated[animation].hide == true) {
         _this.hide();
       };
-      if (callback) {
+      if (jQuery.isFunction(callback)) {
         callback.apply(null, [_this])
       };
     }, options.time)
@@ -36,5 +48,11 @@ var Animated = {
     time: 300,
     hide: false,
     show: false
+  },
+  
+  vendorPrefixFor: function(name) {
+    return $.map(['-webkit-animation-', '-moz-animation-', '-o-animation-', 'animation-'], function(n, i){
+      return n + name
+    })
   }
 }
